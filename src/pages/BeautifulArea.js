272f518e6c266b03e507796037cda82c02326990
Mahmudul-EIS <35486228+Mahmudul-EIS@users.noreply.github.com>
2021-11-09 13:14:20 +0600

    Loading &  Error

--- src/pages/BeautifulArea.js
+++ src/pages/BeautifulArea.js
@@ -1,28 +1,61 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import Loader from "../component/Loader";
 export default function BeautifulARea() {
   const { id } = useParams();
   const url = `https://6188bd7ed0821900178d74f8.mockapi.io/BeautifulArea/${id}`;
-  const [area, setArea] = useState(null);
+  const [area, setArea] = useState({
+    loading: false,
+    data: null,
+    error: false,
+  });
 
   useEffect(() => {
-    axios.get(url).then((response) => {
-      setArea(response.data);
+    setArea({
+      loading: true,
+      data: null,
+      error: false,
     });
+    axios
+      .get(url)
+      .then((response) => {
+        setArea({
+          loading: false,
+          data: response.data,
+          error: false,
+        });
+      })
+      .catch(() => {
+        setArea({
+          loading: false,
+          data: null,
+          error: true,
+        });
+      });
   }, [url]);
 
-  if (area) {
+  if (area.loading) {
+    return <Loader />;
+  }
+
+  if (area.error) {
+    return (
+      <p className="text-center">There has an error, Please try again Latter</p>
+    );
+  }
+
+  if (area.data) {
     return (
       <div className="m-auto w-4/5 md:w-2/4 mt-6">
         <img
           className="rounded mb-4 w-full"
-          src={area.Images}
-          alt={area.name}
+          src={area.data.Images}
+          alt={area.data.name}
         />
-        <h5 className=" text-red-600 text-lg mb-2">{area.price}</h5>
-        <h4 className="text-2xl mb-2 font-semibold">{area.name}</h4>
-        <p className=" text-justify">{area.Description}</p>
+        <h5 className=" text-red-600 text-lg mb-2">{area.data.price}</h5>
+        <h4 className="text-2xl mb-2 font-semibold">{area.data.name}</h4>
+        <p className=" text-justify">{area.data.Description}</p>
       </div>
     );
   }
